f134cf27b1ae7ab255ea325c07c8d8ec6907a236
derek <derekle.creative@gmail.com>
2021-01-20 03:45:32 -0600

    commit tree 0a30bcea3e31166ab8b748367aff0917ca7db0c3

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(num)
+    if num <= 1
+        return false
+    end
+    numSqrt = Math.sqrt(num)
+    testArray = (2..numSqrt).to_a
+    testArray.none?{|i| (num % i) == 0}
+end
